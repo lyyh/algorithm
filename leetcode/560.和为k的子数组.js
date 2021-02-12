139f50a83006e9d069e13ddf63a3a8cc5602114f
youqi <youqi@pinduoduo.com>
2021-02-12 17:23:55 +0800

    feat: 2.11

--- leetcode/560.和为k的子数组.js
+++ leetcode/560.和为k的子数组.js
@@ -10,8 +10,25 @@
  * @param {number} k
  * @return {number}
  */
-var subarraySum = function(nums, k) {
 
-};
+ var subarraySum = function (nums,k) {
+    var n = nums.length
+    var map = new Map()
+    map.set(0,1)
+    var ans = 0,curSum = 0
+    for (var i = 0; i < n; i++) {
+        curSum += nums[i];
+        // 这是我们想找的前缀和 nums[0..j]
+        var preSum = curSum - k;
+        // 如果前面有这个前缀和，则直接更新答案
+        if (map.has(preSum)){
+            ans += map.get(preSum);
+        }
+            
+        // 把前缀和 nums[0..i] 加入并记录出现次数
+        map.set(curSum, 
+            map.get(curSum) || 0 + 1);
+    }
+    return ans;
+ }
 // @lc code=end
-
