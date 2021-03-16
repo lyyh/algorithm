ddf08f574eb547b88fd4d95ed1b8079e536366b3
youqi <youqi@pinduoduo.com>
2021-03-16 22:30:15 +0800

    feat: update

--- leetcode/102.二叉树的层序遍历.js
+++ leetcode/102.二叉树的层序遍历.js
@@ -16,30 +16,29 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrder = function (root) {
+var levelOrder = function(root){
     var res = []
-    var nodes = [];
-    if (!root) {
-        return res
+    var nodes = []
+    if(!root){
+        return nodes
     }
-
     nodes.push(root)
-    while (nodes.length) {
-        var size = nodes.length;
-        var list = [];
-        for (var i = 0; i < size; i++) {
-            var node = nodes.shift();
-            if(!node)continue;
+    while(nodes.length){
+        var size = nodes.length
+        var list = []
+        for(var i = 0;i < size;i++){
+            var node = nodes.shift()
+            if(!node)continue
             list.push(node.val)
-            if (node.left) {
+            if(node.left){
                 nodes.push(node.left)
             }
-            if (node.right) {
+            if(node.right){
                 nodes.push(node.right)
-            }
+            }           
         }
         res.push(list)
     }
     return res
-};
+}
 // @lc code=end
